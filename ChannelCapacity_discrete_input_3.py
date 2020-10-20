4c77142b7b7b12fc180e9bf114ff4235134868c6
FFuchsbe <69785497+FFuchsbe@users.noreply.github.com>
2020-10-20 09:31:26 +0200

    Update ChannelCapacity_discrete_input_3.py

--- ChannelCapacity_discrete_input_3.py
+++ ChannelCapacity_discrete_input_3.py
@@ -11,7 +11,7 @@ from FlowCytometryTools import FCMeasurement
 dir_path = os.path.dirname(os.path.realpath(__file__))   # set the working directory to the location of the Python script file. 
 
 
-folder_name = "04.05.20 My Cells/a dose-response"  # drop .fcs files into (sub)directory. The script will use all .fcs files found in a folder
+folder_name = "Channel capacity/Dose TNF-a"  # drop .fcs files into (sub)directory. The script will use all .fcs files found in a folder
 file_names = os.listdir(folder_name)  #folder including fcs files
 elongated_path_name = dir_path +"\\" +folder_name
 
